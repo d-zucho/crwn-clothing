72b43f4994d2c5a8135855616b72317e09f48265
D_Zucho <danny.nico.dev@outlook.com>
2022-04-30 03:22:31 -0700

    start initial skeleton and styling homepage category menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import './App.css'
+import Category from './components/Category/Category'
+import './App.scss'
+
+const categories = [
+  {
+    id: 1,
+    title: 'hats',
+    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+  },
+  {
+    id: 2,
+    title: 'jackets',
+    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+  },
+  {
+    id: 3,
+    title: 'sneakers',
+    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+  },
+  {
+    id: 4,
+    title: 'womens',
+    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+  },
+  {
+    id: 5,
+    title: 'mens',
+    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+  },
+]
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1>blank</h1>
-      </header>
+    <div className='categories-container'>
+      {categories.map((category) => (
+        //* category container = inside Category component
+        <Category key={category.id} category={category} />
+      ))}
     </div>
   )
 }
